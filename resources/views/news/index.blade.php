e48b5dd917d1aa6367bc2a36fda737f844f989d2
EC2 Default User <ec2-user@ip-172-31-42-24.ap-northeast-1.compute.internal>
2021-11-26 05:54:41 +0000

    完了

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -10,9 +10,9 @@
                         <div class="col-md-6">
                             <div class="caption mx-auto">
                                 <div class="image">
-                                    @if ($headline->image_path)
-                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
-                                    @endif
+                                     @if ($headline->image_path)
+                                        <img src="{{ $headline->image_path }}">
+                                     @endif
                                 </div>
                                 <div class="title p-2">
                                     <h1>{{ str_limit($headline->title, 70) }}</h1>
@@ -45,8 +45,8 @@
                             </div>
                             <div class="image col-md-6 text-right mt-4">
                                 @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
-                                @endif
+                                    <img src="{{ $post->image_path }}">
+                                 @endif
                             </div>
                         </div>
                     </div>
